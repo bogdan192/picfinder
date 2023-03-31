f478f82d2384de70bc79aa9c174dd1c4bfb83dae
unknown <bogdan192@yahoo.com>
2023-03-31 14:09:25 +0300

    First tests

---
+++ README.md
@@ -0,0 +1,23 @@
+# Smoke tests for picfinder.ai
+
+Tests to open the page, check landing page elements and do a simple search
+
+- Runs only on chromium
+- Support for **headless and headed** execution (run with --hedless for speed).
+ 
+
+## How to install
+
+- Install python 3 [Download Page](https://www.python.org/downloads/)
+- Install playwright for Python (both module and browsers) see [Installation guide](https://playwright.dev/python/docs/intro)
+
+## How to run
+- For headed execution (see the browser) 
+
+python3 -m pytest --html=report.html --self-contained-html --headed tests.py
+
+- For headless replace the--headed parameter with --headless
+
+## Documentation
+
+See on [playwright.dev](https://playwright.dev/python/docs/test-runners) for examples and more detailed information.
---
+++ tests.py
@@ -0,0 +1,41 @@
+import pytest
+from playwright.sync_api import Page
+
+
+URL = 'https://picfinder.ai/'
+SELECTORS = {'search_bar': '#form-search-bar\[search-box\]',
+             'spiners': '.grid-wrapper > div > .s-result-grid-item > .s-result-grid-item-hover'}
+STRINGS = {'motto': 'Infinite image generation powered by AI.',
+           'copywright': 'Copyright © 2023 PicFinder.ai All rights reserved.',
+           'donate': 'Your generosity keeps us going.',
+           'in_touch': 'Get in touch',
+           'images_generated': 'Images Generated:'}
+
+
+def test_landing_page(page: Page) -> None:
+    page.goto(URL)
+    search_bar = page.query_selector(SELECTORS['search_bar'])
+    assert 'Try ' in search_bar.get_attribute('placeholder')
+    page.get_by_role("heading").click()
+    page.get_by_text(STRINGS['motto']).click()
+    page.get_by_text(STRINGS['images_generated']).click()
+    page.get_by_role("contentinfo").get_by_text(STRINGS['copywright']).click()
+    page.get_by_role("contentinfo").get_by_text(STRINGS['donate']).click()
+    page.get_by_role("link", name="DONATE").click()
+    page.get_by_role("dialog").click()
+    page.get_by_role("link", name=STRINGS['in_touch']).press("Escape")
+
+
+
+@pytest.mark.parametrize("search_string", ['cat'])
+def test_search(search_string, page: Page) -> None:
+    page.goto(URL)
+    search_bar = page.wait_for_selector(SELECTORS['search_bar'])
+    search_bar.type(search_string)
+    page.locator("#aux-buttons").get_by_role("link").click()
+    results = page.wait_for_selector('.s-result')
+    assert results is not None
+    results_section = page.wait_for_selector('.grid-wrapper')
+    assert results_section is not None
+    page.wait_for_selector(SELECTORS['spiners']).is_visible()
+
